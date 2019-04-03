b62ad85f30dd6c21cbf50cf2c6dd420ec2ff0517
ollehints <olle.hints@ttu.ee>
2019-04-04 00:08:39 +0300

    Estonian map layers

--- src/components/partial/MapComponent.vue
+++ src/components/partial/MapComponent.vue
@@ -48,25 +48,16 @@
     data() {
       return {
         map: null,
-        zoom: 8,
-        crs: new L.Proj.CRS('EPSG:3301',
-          '+proj=lcc +lat_1=59.33333333333334 +lat_2=58 +lat_0=57.51755393055556 +lon_0=24 +x_0=500000 +y_0=6375000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs',
-          {
-            resolutions: [
-              1024, 512, 256, 128,
-              64, 32, 16, 8, 4, 2, 1, 0.5
-            ],
-            transformation: new L.Transformation(1, -40500, -1, 7017000)
-          }),
-        proj1: '+proj=lcc +lat_1=59.33333333333334 +lat_2=58 +lat_0=57.51755393055556 +lon_0=24 +x_0=500000 +y_0=6375000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs',
-        proj2:'+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs ',
+        zoom: 10,
         center: this.isValidLocation(this.location) ? L.latLng(this.location) : L.latLng(58.5, 25.5),
         marker: this.isValidLocation(this.location) ? L.latLng(this.location) : null,
         tileProviders: [
-          {
+         {
             name: 'OpenStreetMap',
             visible: true,
             continuousWorld: true,
+            maxZoom: 8,
+            minZoom: 1,
             url: 'https://api.tiles.mapbox.com/v4/mapbox.light/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoia3V1dG9iaW5lIiwiYSI6ImNpZWlxdXAzcjAwM2Nzd204enJvN2NieXYifQ.tp6-mmPsr95hfIWu3ASz2w',
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
           },
@@ -77,13 +68,48 @@
             url: 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
             attribution: 'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
           },
-          // {
-          //   name: 'Maaamet',
-          //   visible: false,
-          //   continuousWorld: false,
-          //   url: 'http://tiles.maaamet.ee/tm/tms/1.0.0/foto/{z}/{x}/{-y}.png',
-          //   attribution: 'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-          // }
+          {
+              name: 'Maaameti fotokaart',
+              visible: false,
+              tms: true,
+              worldCopyJump: true,
+              detectRetina: true,
+              maxZoom: 18,
+              minZoom: 10,
+              zIndex: 2,
+              updateWhenIdle: true,
+              continuousWorld: true,
+              url: 'https://tiles.maaamet.ee/tm/tms/1.0.0/foto@GMC/{z}/{x}/{-y}.png&ASUTUS=TALTECH&KESKKOND=LIVE&IS=SARV',
+              attribution: 'Eesti kaardid: <a  href=\'http://www.maaamet.ee/\'>Maa-amet</a>'
+            },
+            {
+                name: 'Maaameti hübriidkaart',
+                visible: false,
+                tms: true,
+                worldCopyJump: true,
+                detectRetina: true,
+                maxZoom: 18,
+                minZoom: 10,
+                zIndex: 1,
+                updateWhenIdle: true,
+                continuousWorld: true,
+                url: 'https://tiles.maaamet.ee/tm/tms/1.0.0/hybriid@GMC/{z}/{x}/{-y}.png&ASUTUS=TALTECH&KESKKOND=LIVE&IS=SARV',
+                attribution: 'Eesti kaardid: <a  href=\'http://www.maaamet.ee/\'>Maa-amet</a>'
+            },
+            {
+                name: 'Maaameti kaart',
+                visible: true,
+                tms: true,
+                worldCopyJump: true,
+                detectRetina: true,
+                maxZoom: 18,
+                minZoom: 10,
+                zIndex: 1,
+                updateWhenIdle: true,
+                continuousWorld: true,
+                url: 'https://tiles.maaamet.ee/tm/tms/1.0.0/kaart@GMC/{z}/{x}/{-y}.png&ASUTUS=TALTECH&KESKKOND=LIVE&IS=SARV',
+                attribution: 'Eesti kaardid: <a  href=\'http://www.maaamet.ee/\'>Maa-amet</a>'
+            }
         ],
       }
     },
@@ -98,7 +124,9 @@
 
         } else {
           this.marker = null
+          //this.zoom = 10
           this.center = L.latLng(58.5, 25.5)
+          this.zoom = 10
         }
       },
     },
@@ -109,11 +137,7 @@
         this.map = this.$refs.map.mapObject;
         let vm = this
         this.map.on('baselayerchange', function (event) {
-          if(event.name === 'Maaamet') {
-            vm.applyProjection()
-          } else {
-            vm.setDefault()
-          }
+        	vm.setDefaultProjection()
         });
       })
     },
@@ -139,21 +163,8 @@
         this.$emit('get-location', event.latlng)
       },
 
-      transformCoordFromEPSG3301ToWSG(val){
-        return proj4(proj4('EPSG:3301'),proj4('WGS84'),[parseFloat(val.lat), parseFloat(val.lng)]);
-      },
-      transformCoordFromWSGToEPSG3301(val){
-        return proj4(proj4('WGS84'),proj4('EPSG:3301')[parseFloat(val.lng),parseFloat(val.lat)]);
-      },
-
-      applyProjection(){
-        this.$emit('get-location', this.transformCoordFromEPSG3301ToWSG(this.location))
-
-        this.zoom = 2
-      },
-
       setDefaultProjection(){
-        this.zoom = 8
+        this.zoom = 10
       }
 
     }
