391bef18d0637234736f7a6e8c398ae148974211
Enar <enmust@ttu.ee>
2019-04-17 13:46:00 +0300

    #95, small change

--- src/components/library/Library.vue
+++ src/components/library/Library.vue
@@ -1,112 +1,122 @@
 <template>
   <div class="library">
-    <spinner v-show="sendingData" class="loading-overlay" size="massive" :message="$route.meta.isEdit ? $t('edit.overlayLoading'):$t('add.overlay')"></spinner>
+    <spinner v-show="sendingData" class="loading-overlay" size="massive"
+             :message="$route.meta.isEdit ? $t('edit.overlayLoading'):$t('add.overlay')"></spinner>
 
-    <!-- AUTHOR AND YEAR -->
-    <div class="row">
-      <div class="col-sm-2">
-        <label :for="`author`">{{ $t('library.author_txt') }}:</label>
+    <fieldset class="border p-2 mb-2">
+      <legend class="w-auto" style="font-size: large;">
+        {{ $t('library.generalInfo') }}
+        <font-awesome-icon icon="project-diagram"/>
+      </legend>
+
+      <!-- AUTHOR AND YEAR -->
+      <div class="row">
+        <div class="col-sm-2">
+          <label :for="`author`">{{ $t('library.author_txt') }}:</label>
+        </div>
+
+        <div class="col-sm-4 mb-2">
+          <vue-multiselect id="author" class="align-middle"
+                           v-model="library.author"
+                           deselect-label="Can't remove this value"
+                           label="agent"
+                           track-by="id"
+                           :placeholder="$t('add.inputs.autocomplete')"
+                           :loading="autocomplete.loaders.agent"
+                           :options="autocomplete.agent"
+                           @search-change="autcompleteAgentSearch"
+                           :internal-search="false"
+                           :preserve-search="true"
+                           :allow-empty="false"
+                           :open-direction="'bottom'">
+            <template slot="noResult"><b>{{ $t('messages.inputNoResults') }}</b></template>
+          </vue-multiselect>
+        </div>
+
+
+        <div class="col-sm-2">
+          <label :for="`year`">{{ $t('library.year') }}:</label>
+        </div>
+
+        <div class="col-sm-4 mb-2">
+          <b-form-input id="year" v-model="library.year" type="number"></b-form-input>
+        </div>
       </div>
 
-      <div class="col-sm-4 mb-2">
-        <vue-multiselect id="author" class="align-middle"
-                         v-model="library.author"
-                         deselect-label="Can't remove this value"
-                         label="agent"
-                         track-by="id"
-                         :placeholder="$t('add.inputs.autocomplete')"
-                         :loading="autocomplete.loaders.agent"
-                         :options="autocomplete.agent"
-                         @search-change="autcompleteAgentSearch"
-                         :internal-search="false"
-                         :preserve-search="true"
-                         :allow-empty="false"
-                         :open-direction="'bottom'">
-          <template slot="noResult"><b>{{ $t('messages.inputNoResults') }}</b></template>
-        </vue-multiselect>
+      <!-- AUTHORS -->
+      <div class="row">
+        <div class="col-sm-2">
+          <label :for="`author_txt`">{{ $t('library.author_txt') }}:</label>
+        </div>
+
+        <div class="col-sm-10 mb-2">
+          <b-form-input id="author_txt" v-model="library.author_txt" type="text"></b-form-input>
+        </div>
       </div>
 
+      <!-- TITLE -->
+      <div class="row">
+        <div class="col-sm-2">
+          <label :for="`title`">{{ $t('library.title') }}:</label>
+        </div>
 
-      <div class="col-sm-2">
-        <label :for="`year`">{{ $t('library.year') }}:</label>
+        <div class="col-sm-10 mb-2">
+          <b-form-input id="title" v-model="library.title" type="text"></b-form-input>
+        </div>
       </div>
 
-      <div class="col-sm-4 mb-2">
-        <b-form-input id="year" v-model="library.year" type="number"></b-form-input>
-      </div>
-    </div>
+      <!-- TITLE_EN -->
+      <div class="row">
+        <div class="col-sm-2">
+          <label :for="`title_en`">{{ $t('library.title_en') }}:</label>
+        </div>
 
-    <!-- AUTHORS -->
-    <div class="row">
-      <div class="col-sm-2">
-        <label :for="`author_txt`">{{ $t('library.author_txt') }}:</label>
-      </div>
-
-      <div class="col-sm-10 mb-2">
-        <b-form-input id="author_txt" v-model="library.author_txt" type="text"></b-form-input>
-      </div>
-    </div>
-
-    <!-- TITLE -->
-    <div class="row">
-      <div class="col-sm-2">
-        <label :for="`title`">{{ $t('library.title') }}:</label>
+        <div class="col-sm-10 mb-2">
+          <b-form-input id="title_en" v-model="library.title_en" type="text"></b-form-input>
+        </div>
       </div>
 
-      <div class="col-sm-10 mb-2">
-        <b-form-input id="title" v-model="library.title" type="text"></b-form-input>
-      </div>
-    </div>
+      <!-- KEYWORDS -->
+      <div class="row">
+        <div class="col-sm-2">
+          <label :for="`keywords`">{{ $t('library.keywords') }}:</label>
+        </div>
 
-    <!-- TITLE_EN -->
-    <div class="row">
-      <div class="col-sm-2">
-        <label :for="`title_en`">{{ $t('library.title_en') }}:</label>
-      </div>
-
-      <div class="col-sm-10 mb-2">
-        <b-form-input id="title_en" v-model="library.title_en" type="text"></b-form-input>
-      </div>
-    </div>
-
-    <!-- KEYWORDS -->
-    <div class="row">
-      <div class="col-sm-2">
-        <label :for="`keywords`">{{ $t('library.keywords') }}:</label>
+        <div class="col-sm-10 mb-2">
+          <b-form-input id="keywords" v-model="library.keywords" type="text"></b-form-input>
+        </div>
       </div>
 
-      <div class="col-sm-10 mb-2">
-        <b-form-input id="keywords" v-model="library.keywords" type="text"></b-form-input>
-      </div>
-    </div>
+      <!-- ABSTRACT AND ABSTRACT_EN -->
+      <div class="row">
+        <div class="col-sm-2">
+          <label :for="`abstract`">{{ $t('library.abstract') }}:</label>
+        </div>
 
-    <!-- ABSTRACT AND ABSTRACT_EN -->
-    <div class="row">
-      <div class="col-sm-2">
-        <label :for="`abstract`">{{ $t('library.abstract') }}:</label>
-      </div>
-
-      <div class="col-sm-4 mb-2">
-        <b-form-textarea id="abstract" v-model="library.abstract" type="text" size="sm"
-                         :rows="1" :max-rows="20"></b-form-textarea>
-      </div>
+        <div class="col-sm-4 mb-2">
+          <b-form-textarea id="abstract" v-model="library.abstract" type="text" size="sm"
+                           :rows="1" :max-rows="20"></b-form-textarea>
+        </div>
 
 
-      <div class="col-sm-2">
-        <label :for="`abstract_en`">{{ $t('library.abstract_en') }}:</label>
+        <div class="col-sm-2">
+          <label :for="`abstract_en`">{{ $t('library.abstract_en') }}:</label>
+        </div>
+
+        <div class="col-sm-4 mb-2">
+          <b-form-textarea id="abstract_en" v-model="library.abstract_en" type="text" size="sm"
+                           :rows="1" :max-rows="20"></b-form-textarea>
+        </div>
       </div>
 
-      <div class="col-sm-4 mb-2">
-        <b-form-textarea id="abstract_en" v-model="library.abstract_en" type="text" size="sm"
-                         :rows="1" :max-rows="20"></b-form-textarea>
-      </div>
-    </div>
+    </fieldset>
 
     <!-- LIBRARY MEMBERS -->
     <fieldset class="border p-2 mb-2">
       <legend class="w-auto" style="font-size: large;">
         {{ $t('library.libraryAgent') }}
-        <font-awesome-icon icon="user-friends"/></legend>
+        <font-awesome-icon icon="user-friends"/>
+      </legend>
       <div class="row">
 
         <div class="col-11 mb-2 mr-0">
@@ -125,7 +135,8 @@
         </div>
 
         <div class="col-1 mb-2 ml-0 pl-0">
-          <button class="btn btn-outline-danger" :title="$t('add.inputs.keywordsRemove')" :disabled="!isDefinedAndNotEmpty(relatedData.library_agent)"
+          <button class="btn btn-outline-danger" :title="$t('add.inputs.keywordsRemove')"
+                  :disabled="!isDefinedAndNotEmpty(relatedData.library_agent)"
                   @click="relatedData.library_agent = []">
             <font-awesome-icon icon="trash-alt"></font-awesome-icon>
           </button>
@@ -137,7 +148,7 @@
     <fieldset class="border p-2 mb-2" v-if="$route.meta.isEdit">
       <legend class="w-auto" style="font-size: large;">
         {{ $t('library.relatedTables.libraryReference') }}
-        <font-awesome-icon icon="list-ol" />
+        <font-awesome-icon icon="list-ol"/>
       </legend>
 
       <div class="row">
@@ -148,7 +159,8 @@
         </div>
       </div>
 
-      <library-reference-list-view v-if="showListView" :data="relatedData.library_reference"></library-reference-list-view>
+      <library-reference-list-view v-if="showListView"
+                                   :data="relatedData.library_reference"></library-reference-list-view>
     </fieldset>
 
     <!-- SHOWING RELATED_DATA -->
@@ -156,7 +168,8 @@
       <div class="col mt-4">
         <ul class="nav nav-tabs tab-links mb-3" style="flex-wrap: nowrap !important">
           <li class="nav-item">
-            <a href="#" v-on:click.prevent="setActiveTab('library_reference')" class="nav-link" :class="{ active: activeTab === 'library_reference' }">
+            <a href="#" v-on:click.prevent="setActiveTab('library_reference')" class="nav-link"
+               :class="{ active: activeTab === 'library_reference' }">
               {{ $t('library.relatedTables.libraryReference') }}
             </a>
           </li>
@@ -167,9 +180,11 @@
           <!--<button class="btn float-left btn-sm btn-outline-success mr-2 mb-2 pl-4 pr-4"-->
           <!--:disabled="sendingData" @click="addRelatedData(activeTab)">{{$t('add.newRelation')}}</button>-->
           <!--</div>-->
-          <div class="col pagination-center" v-if="relatedData[activeTab] !== null && relatedData[activeTab].length > 0">
+          <div class="col pagination-center"
+               v-if="relatedData[activeTab] !== null && relatedData[activeTab].length > 0">
             <b-pagination
-              size="sm" align="right" :limit="5" :hide-ellipsis="true" :total-rows="relatedData.count[activeTab]" v-model="relatedData.page[activeTab]" :per-page="100">
+              size="sm" align="right" :limit="5" :hide-ellipsis="true" :total-rows="relatedData.count[activeTab]"
+              v-model="relatedData.page[activeTab]" :per-page="100">
             </b-pagination>
           </div>
         </div>
@@ -188,11 +203,15 @@
     <div class="row mt-3 mb-3">
       <div class="col">
         <button class="btn btn-success mr-2 mb-2" :disabled="sendingData" @click="add(false, 'library')">
-          {{ $t($route.meta.isEdit? 'edit.buttons.save':'add.buttons.add') }}</button>
+          {{ $t($route.meta.isEdit? 'edit.buttons.save':'add.buttons.add') }}
+        </button>
         <button class="btn btn-success mr-2 mb-2" :disabled="sendingData" @click="add(true, 'library')">
-          {{ $t($route.meta.isEdit? 'edit.buttons.saveAndContinue':'add.buttons.addAnother') }}</button>
-        <button class="btn btn-danger mr-2 mb-2" :disabled="sendingData" @click="$route.meta.isEdit ? leaveFromEditView('library') : reset('library')">
-          {{ $t($route.meta.isEdit? 'edit.buttons.cancelWithoutSaving':'add.buttons.clearFields') }}</button>
+          {{ $t($route.meta.isEdit? 'edit.buttons.saveAndContinue':'add.buttons.addAnother') }}
+        </button>
+        <button class="btn btn-danger mr-2 mb-2" :disabled="sendingData"
+                @click="$route.meta.isEdit ? leaveFromEditView('library') : reset('library')">
+          {{ $t($route.meta.isEdit? 'edit.buttons.cancelWithoutSaving':'add.buttons.clearFields') }}
+        </button>
       </div>
     </div>
 
@@ -201,16 +220,16 @@
 
 <script>
   import Spinner from 'vue-simple-spinner'
-  import { library } from '@fortawesome/fontawesome-svg-core'
+  import {library} from '@fortawesome/fontawesome-svg-core'
   import VueMultiselect from 'vue-multiselect'
-  import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+  import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
   import {faTimes} from '@fortawesome/free-solid-svg-icons'
-  import { fetchLibrary, fetchLibraries, fetchLibraryReference, fetchLibraryAgent } from "../../assets/js/api/apiCalls";
+  import {fetchLibrary, fetchLibraries, fetchLibraryReference, fetchLibraryAgent} from "../../assets/js/api/apiCalls";
   import cloneDeep from 'lodash/cloneDeep'
-  import { toastSuccess, toastError } from "@/assets/js/iziToast/iziToast";
-  import formManipulation  from './../mixins/formManipulation'
-  import autocompleteFieldManipulation  from './../mixins/autocompleFormManipulation'
-  import copyForm  from './../mixins/copyForm'
+  import {toastSuccess, toastError} from "@/assets/js/iziToast/iziToast";
+  import formManipulation from './../mixins/formManipulation'
+  import autocompleteFieldManipulation from './../mixins/autocompleFormManipulation'
+  import copyForm from './../mixins/copyForm'
   import LibraryReference from "./relatedTables/LibraryReference";
   import LibraryReferenceListView from "./relatedTables/LibraryReferenceListView";
 
@@ -225,11 +244,15 @@
       VueMultiselect,
       Spinner,
     },
-    mixins: [formManipulation,copyForm,autocompleteFieldManipulation],
+    mixins: [formManipulation, copyForm, autocompleteFieldManipulation],
 
-    data() { return this.setInitialData() },
+    data() {
+      return this.setInitialData()
+    },
 
-    created() { this.loadFullInfo() },
+    created() {
+      this.loadFullInfo()
+    },
 
     watch: {
       '$route.params.id': {
@@ -242,7 +265,9 @@
 
     methods: {
 
-      setTab(type) { this.activeTab = type },
+      setTab(type) {
+        this.activeTab = type
+      },
 
       setInitialData() {
         return {
@@ -280,16 +305,16 @@
           this.relatedData.library_agent = this.handleResponse(response);
         })
 
-        if(this.$route.meta.isEdit) {
+        if (this.$route.meta.isEdit) {
           this.sendingData = true;
           fetchLibrary(this.$route.params.id).then(response => {
             let handledResponse = this.handleResponse(response);
-            if(handledResponse.length > 0) {
+            if (handledResponse.length > 0) {
               this.library = this.handleResponse(response)[0];
               this.fillAutocompleteFields(this.library)
-              this.removeUnnecessaryFields(this.library,this.copyFields);
-              this.$emit('data-loaded',this.library)
-              this.$emit('set-object','library')
+              this.removeUnnecessaryFields(this.library, this.copyFields);
+              this.$emit('data-loaded', this.library)
+              this.$emit('set-object', 'library')
               this.sendingData = false;
               this.getListRecords('library')
             } else {
@@ -302,30 +327,30 @@
             this.loadRelatedData(entity);
           });
 
-          this.$on('tab-changed',this.setTab);
+          this.$on('tab-changed', this.setTab);
 
-          this.$on('related-data-modified',this.editRelatedData);
-          this.$on('related-data-added',this.addRelatedData);
-          this.$on('edit-row',this.editRow);
-          this.$on('allow-remove-row',this.allowRemove);
+          this.$on('related-data-modified', this.editRelatedData);
+          this.$on('related-data-added', this.addRelatedData);
+          this.$on('edit-row', this.editRow);
+          this.$on('allow-remove-row', this.allowRemove);
 
-          this.$emit('related-data-info',this.tabs);
+          this.$emit('related-data-info', this.tabs);
 
           this.setActiveTab('library_reference')
         }
       },
 
-      setDefaultRelatedData(){
+      setDefaultRelatedData() {
         return {
-          library_reference:[],
+          library_reference: [],
           library_agent: [],
           copyFields: {
-            library_reference: ['reference__reference','keywords','remarks'],
+            library_reference: ['reference__reference', 'keywords', 'remarks'],
           },
           insert: {
             library_reference: {}
           },
-          page : {
+          page: {
             library_reference: 1,
             library_agent: 1,
           },
@@ -347,20 +372,20 @@
         return JSON.stringify(uploadableObject)
       },
 
-      fillAutocompleteFields(obj){
-        this.library.author = { agent: obj.author__agent, id: obj.author }
+      fillAutocompleteFields(obj) {
+        this.library.author = {agent: obj.author__agent, id: obj.author}
       },
 
-      fillRelatedDataAutocompleteFields(obj){
-        obj.reference = { reference:obj.reference__reference, id:obj.reference}
+      fillRelatedDataAutocompleteFields(obj) {
+        obj.reference = {reference: obj.reference__reference, id: obj.reference}
         // obj.library_agent = { agent: obj.agent__agent, id: obj.agent }
         return obj
       },
 
-      loadRelatedData(object){
+      loadRelatedData(object) {
         let query;
 
-        if(object === 'library_reference') {
+        if (object === 'library_reference') {
           query = fetchLibraryReference(this.$route.params.id, this.relatedData.page.library_reference)
         }
         return new Promise(resolve => {
@@ -373,11 +398,11 @@
       },
 
       //check required fields for related data
-      checkRequiredFields(type){
+      checkRequiredFields(type) {
 
       },
 
-      formatRelatedData(objectToUpload)  {
+      formatRelatedData(objectToUpload) {
         let uploadableObject = cloneDeep(objectToUpload);
         uploadableObject.library = this.library.id;
 
