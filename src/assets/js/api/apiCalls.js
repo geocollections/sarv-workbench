3e8888ce2dde609c4e83c08634157143bc8d9e15
Enar <enmust@ttu.ee>
2019-05-16 23:20:30 +0300

    #184, removed test url

--- src/assets/js/api/apiCalls.js
+++ src/assets/js/api/apiCalls.js
@@ -365,9 +365,8 @@ export function fetchSamples(data, agent) {
     return fetch(`sample/?or_search=agent_collected__id:${agent.id};user_added:${agent.user};owner__id:${agent.id}&${searchFields}&page=${data.page}&paginate_by=${data.paginateBy}&order_by=${data.orderBy}&fields=${fields}&format=json`)
   } else {
 	  // console.log(agent);
-    // TODO: uncomment correct fetch
-    // return fetch(`sample/?or_search=agent_collected__id:${agent.id};user_added:${agent.user};owner__id:${agent.id}&page=${data.page}&paginate_by=${data.paginateBy}&order_by=${data.orderBy}&fields=${fields}&format=json`)
-    return fetch(`sample/?page=${data.page}&paginate_by=${data.paginateBy}&order_by=${data.orderBy}&fields=${fields}&format=json`)
+    return fetch(`sample/?or_search=agent_collected__id:${agent.id};user_added:${agent.user};owner__id:${agent.id}&page=${data.page}&paginate_by=${data.paginateBy}&order_by=${data.orderBy}&fields=${fields}&format=json`)
+    // return fetch(`sample/?page=${data.page}&paginate_by=${data.paginateBy}&order_by=${data.orderBy}&fields=${fields}&format=json`)
   }
 }
 export function fetchSampleAnalysis(id,page = 1) {
