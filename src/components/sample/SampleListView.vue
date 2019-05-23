0ebbeaf60166dd789da7e1b625150770a721231a
Enar <enmust@ttu.ee>
2019-05-23 16:38:06 +0300

    #184, code from old app

--- src/components/sample/SampleListView.vue
+++ src/components/sample/SampleListView.vue
@@ -2,6 +2,28 @@
   <div class="row" v-if="data.length > 0">
 
 
+<!--    SHOULD USE THIS (from the old app)-->
+<!--    <div class="label2" v-for="(entity, index) in data">-->
+<!--      <div class="label2_head">-->
+<!--        <div style="float: left;" v-translate="{et: entity.database__name, en: entity.database__name_en}"></div>-->
+<!--        <div style="float: right; text-align: right; padding-right: 1mm; margin-top: -0.5mm; font-size: 1.0em">ID: {{ entity.id }}</div>-->
+<!--      </div>-->
+<!--      <div class="label2_row_name" style="font-size: 1.4em; font-weight: bold;text-align: right; ">TEST proov</div>-->
+<!--      <div class="label2_row2">-->
+<!--        <div style="position: absolute; top: 0; left: 0;"><b>Loc.</b><span style="font-size: 1.1em;">  </span></div>-->
+<!--        <div style="position: absolute; bottom: 0; right: 0;"><span style="font-size: 1.1em; font-weight: normal;"></span></div>-->
+<!--      </div>-->
+<!--      <div class="label2_row"><b>Strat.</b><span style="font-size: 1.1em;">   </span></div>-->
+<!--      <div class="label2_row3" style="margin-right: 11mm;">-->
+<!--        <div style="float: left;"><b>Coll.</b><span style="font-size: 1.1em;"> </span></div>-->
+<!--        <div style="float: right;"> </div>-->
+<!--        <div style="background-color: #fff; border-top: solid 0.25pt #000; border-left: solid 0.25pt #000;position:absolute; bottom: 0mm; right:0mm; padding: 1mm;"><img style="width: 8mm;" src="tmp/sample_145235.png"></div>-->
+<!--      </div>-->
+<!--    </div>-->
+
+
+
+
     <div class="col-12 col-sm-6 col-lg-4 col-xl-3 pl-0 pr-1 mb-1" v-for="(entity, index) in data">
 
       <b-card
@@ -75,5 +97,55 @@
   .sample-card-body {
     font-size: 12px;
   }
+
+  /* STYLES FROM OLD APP */
+  .label2 {
+    position: relative;
+    float: left;
+    height: 38mm;
+    width: 58mm;
+    border: solid 1pt #000;
+    padding: 1mm;
+    margin: 0.5mm;
+    font-size: 9pt;
+    font-family: "open sans", arial, sans-serif;
+    background-color: #fff;
+    page-break-inside: avoid;
+  }
+
+  .label2_head {
+    position: relative;
+    float: left;
+    height: 5mm;
+    width: 59mm;
+    left: -1mm;
+    padding-left: 1mm;
+    border-bottom: solid 0.5pt #000;
+    margin-bottom: 0.5mm;
+    font-size: 8pt;
+    font-weight: bold;
+  }
+
+  .label2_row_name {
+    font-size: 8pt;
+    clear: both;
+    height: 7mm;
+    border-bottom: dotted 0.5pt #000;
+  }
+
+  .label2_row {
+    height: 10mm;
+    border-bottom: dotted 0.5pt #000;
+  }
+
+  .label2_row2 {
+    height: 10mm;
+    border-bottom: dotted 0.5pt #000;
+  }
+
+  .label2_row3 {
+    height: 7mm;
+  }
+
 
 </style>
