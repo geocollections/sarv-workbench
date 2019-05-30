43c8c635cc243ca1c0ad016c1d89a7d6c9e2bd00
Enar <enmust@ttu.ee>
2019-05-30 12:53:30 +0300

    updated attachment image list view

--- src/components/attachment/AttachmentListView.vue
+++ src/components/attachment/AttachmentListView.vue
@@ -1,27 +1,64 @@
 <template>
-  <div class="row" v-if="data.length > 0">
-    <div class="preview" v-for="(entity, index) in data">
+  <div v-if="data.length > 0">
 
-      <div :id="'icon-' + index" class="">
-        <router-link v-if="entity.uuid_filename !== null" :title="$t('edit.editMessage')" :to="{ path: '/attachment/' + entity.id }">
-          <img class="image-preview"
-               v-if="entity.uuid_filename.endsWith('jpg') || entity.uuid_filename.endsWith('jpeg') || entity.uuid_filename.endsWith('png')"
-               :src="composeFileUrl(entity.uuid_filename)"/>
-          <font-awesome-icon v-else class="ico"  size="6x" icon="file"/>
-        </router-link>
+    <vs-switch class="mb-2" v-model="isFancyImageList">
+      <span slot="on">{{ $t('attachments.fancyView') }}</span>
+      <span slot="off">{{ $t('attachments.regularView') }}</span>
+    </vs-switch>
 
-        <router-link :title="$t('edit.editMessage')" :to="{ path: '/attachment/' + entity.id }">
-          <font-awesome-icon v-if="entity.uuid_filename === null" size="5x" :icon="file"/>
-        </router-link>
+    <div v-if="isFancyImageList" class="vs-component vs-images vs-images-hover-default">
+      <ul class="vs-ul-images vs-images--ul">
+        <li class="vs-image custom-size" v-for="(entity, index) in data">
+          <div class="con-vs-image" :id="'icon-' + index">
+            <router-link v-if="entity.uuid_filename !== null" :to="{ path: '/attachment/' + entity.id }">
+              <div class="vs-image--img" :style="'background-image: url(' + composeFileUrl(entity.uuid_filename) + ')'"
+                   v-if="entity.uuid_filename.endsWith('jpg') || entity.uuid_filename.endsWith('jpeg') || entity.uuid_filename.endsWith('png')"></div>
+              <font-awesome-icon v-else class="vs-image--img" size="6x" icon="file"/>
+
+              <div v-else class="vs-image--img"></div>
+            </router-link>
+
+            <router-link :to="{ path: '/attachment/' + entity.id }">
+              <font-awesome-icon v-if="entity.uuid_filename === null" size="6x" icon="file"/>
+            </router-link>
+          </div>
+
+          <b-tooltip :target="'icon-' + index" placement="bottom">
+            <b>ID:</b> {{ entity.id }}<br/>
+            <span v-if="entity.date_created || entity.date_created_free"><b>Date:</b> {{ entity.date_created }} | {{ entity.date_created_free }}<br/></span>
+            <span v-if="entity.author__agent"><b>Author:</b> {{ entity.author__agent }}<br/></span>
+            <span v-if="entity.image_number"><b>Photo:</b> {{ entity.image_number }}<br/></span>
+            <span v-if="entity.original_filename"><b>Filename:</b> {{ entity.original_filename }}<br/></span>
+          </b-tooltip>
+        </li>
+      </ul>
+    </div>
+
+    <div v-if="!isFancyImageList" class="row">
+      <div class="preview" v-for="(entity, index) in data">
+
+        <div :id="'icon-' + index" class="">
+          <router-link v-if="entity.uuid_filename !== null" :title="$t('edit.editMessage')"
+                       :to="{ path: '/attachment/' + entity.id }">
+            <img class="image-preview"
+                 v-if="entity.uuid_filename.endsWith('jpg') || entity.uuid_filename.endsWith('jpeg') || entity.uuid_filename.endsWith('png')"
+                 :src="composeFileUrl(entity.uuid_filename)"/>
+            <font-awesome-icon v-else class="ico" size="6x" icon="file"/>
+          </router-link>
+
+          <router-link :title="$t('edit.editMessage')" :to="{ path: '/attachment/' + entity.id }">
+            <font-awesome-icon v-if="entity.uuid_filename === null" size="5x" :icon="file"/>
+          </router-link>
+        </div>
+
+        <b-tooltip :target="'icon-' + index" placement="auto">
+          <b>ID:</b> {{ entity.id }}<br/>
+          <span v-if="entity.date_created || entity.date_created_free"><b>Date:</b> {{ entity.date_created }} | {{ entity.date_created_free }}<br/></span>
+          <span v-if="entity.author__agent"><b>Author:</b> {{ entity.author__agent }}<br/></span>
+          <span v-if="entity.image_number"><b>Photo:</b> {{ entity.image_number }}<br/></span>
+          <span v-if="entity.original_filename"><b>Filename:</b> {{ entity.original_filename }}<br/></span>
+        </b-tooltip>
       </div>
-
-      <b-tooltip :target="'icon-' + index" placement="auto">
-        <b>ID:</b> {{ entity.id }}<br />
-        <span v-if="entity.date_created || entity.date_created_free"><b>Date:</b> {{ entity.date_created }} | {{ entity.date_created_free }}<br /></span>
-        <span v-if="entity.author__agent"><b>Author:</b> {{ entity.author__agent }}<br /></span>
-        <span v-if="entity.image_number"><b>Photo:</b> {{ entity.image_number }}<br /></span>
-        <span v-if="entity.original_filename"><b>Filename:</b> {{ entity.original_filename }}<br /></span>
-      </b-tooltip>
     </div>
   </div>
 </template>
@@ -36,7 +73,12 @@
       }
     },
     name: "AttachmentListView",
-    mixins: [formManipulation]
+    mixins: [formManipulation],
+    data() {
+      return {
+        isFancyImageList: true
+      }
+    }
   }
 </script>
 
@@ -46,10 +88,20 @@
       max-height: 150px;
       max-width: 150px;
     }
+
+    .custom-size {
+      max-height: 130px;
+      max-width: 130px;
+    }
   }
 
   @media (min-width: 576px) {
     .image-preview {
+      max-height: 200px;
+      max-width: 200px;
+    }
+
+    .custom-size {
       max-height: 200px;
       max-width: 200px;
     }
@@ -60,9 +112,9 @@
     background-color: #fff;
     border: 1px solid #dee2e6;
     border-radius: 0.25rem;
-	padding: 0.2rem;
+    padding: 0.2rem;
     margin: 0.2rem 0.4rem;
-	max-height: 120px;
+    max-height: 120px;
   }
 
   .center {
@@ -79,6 +131,7 @@
     -ms-transform: translate(-50%, -50%);
     transform: translate(-50%, -50%);
   }
+
   .preview .ico {
     padding: 0.2rem;
     margin: 0.2rem 0.4rem;
