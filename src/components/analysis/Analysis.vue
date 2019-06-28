4404319b1a229e8be188e3046fbd28e69f9f796b
Enar <enmust@ttu.ee>
2019-06-28 11:20:57 +0300

    #213 analysis mass update

--- src/components/analysis/Analysis.vue
+++ src/components/analysis/Analysis.vue
@@ -68,7 +68,7 @@
 
             <div class="col-md-6">
               <label class="p-0" :for="`mass`">{{ $t('analysis.mass') }}:</label>
-              <b-form-input id="mass" v-model="analysis.mass" type="text"></b-form-input>
+              <b-form-input id="mass" v-model="analysis.mass" type="number" step="0.1"></b-form-input>
             </div>
           </div>
 
