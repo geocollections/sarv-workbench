41b943bf27829e40478dc864060a02d1878371b6
Enar <enmust@ttu.ee>
2019-11-12 14:42:38 +0200

    #316 continuous deployment update

--- server_dir_update.sh
+++ server_dir_update.sh
@@ -5,19 +5,19 @@ ssh -p 5522 deploy@193.40.102.19 << EOF
 cd /var/www/sarv-edit-dev/ || exit
 
 if [ -d $currentDate/ ]; then
-  echo "--------------------------------"
+  echo "-------------------------------"
   echo "REMOVING: '$currentDate' directory"
   rm -r "$currentDate"/
   echo "Done."
 fi
 
-echo "--------------------------------"
+echo "---------------------------------"
 echo "MAKING: new '$currentDate' directory"
 mkdir "$currentDate"
 echo "Done."
 
 if [ -d js/ ]; then
-  echo "--------------------------------"
+  echo "-------------------------------"
   echo "MOVING: current build to '$currentDate' directory..."
   mv css/ favicon.ico fonts/ img/ index.html js/ "$currentDate"/
   echo "Done."
