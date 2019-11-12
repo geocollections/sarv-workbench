b8b8934c89e00170746fafa81401663c1cf7aeab
Enar <enmust@ttu.ee>
2019-11-12 14:38:43 +0200

    #316 cache update

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bash
+bash server_dir_update.sh
+
+echo "--------------------------------"
+echo "BUILDING: Building project..."
+npm run build
+echo "Done."
+
+echo "--------------------------------"
+echo "MOVING: built files to server..."
+scp -P 5522 -a dist/. deploy@193.40.102.19:/var/www/sarv-edit-dev
+echo "All done."
---
+++ server_dir_update.sh
@@ -0,0 +1,25 @@
+#!/usr/bash
+currentDate=$(date +"%Y-%m-%d")
+
+ssh -p 5522 deploy@193.40.102.19 << EOF
+cd /var/www/sarv-edit-dev/ || exit
+
+if [ -d $currentDate/ ]; then
+  echo "--------------------------------"
+  echo "REMOVING: '$currentDate' directory"
+  rm -r "$currentDate"/
+  echo "Done."
+fi
+
+echo "--------------------------------"
+echo "MAKING: new '$currentDate' directory"
+mkdir "$currentDate"
+echo "Done."
+
+if [ -d js/ ]; then
+  echo "--------------------------------"
+  echo "MOVING: current build to '$currentDate' directory..."
+  mv css/ favicon.ico fonts/ img/ index.html js/ "$currentDate"/
+  echo "Done."
+fi
+EOF
