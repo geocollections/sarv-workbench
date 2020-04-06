faf9a3630ffcda28e6ddc8e7c1026ba2cd98dbb4
Enar <enmust@ttu.ee>
2020-04-06 15:00:05 +0300

    #474 date fix

--- src/components/dashboard/RecentActivity.vue
+++ src/components/dashboard/RecentActivity.vue
@@ -36,7 +36,7 @@
                   <div v-else>{{ entity.table_name }}: {{ entity.row_id }}</div>
                 </td>
                 <td>
-                  {{ entity.time | moment("YYYY-DD-MM HH:mm") }}
+                  {{ entity.time | moment("YYYY-MM-DD HH:mm") }}
                 </td>
               </tr>
             </tbody>
@@ -81,7 +81,7 @@
                   <div v-else>{{ entity.table_name }}: {{ entity.row_id }}</div>
                 </td>
                 <td>
-                  {{ entity.time | moment("YYYY-DD-MM HH:mm") }}
+                  {{ entity.time | moment("YYYY-MM-DD HH:mm") }}
                 </td>
               </tr>
             </tbody>
