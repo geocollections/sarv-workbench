1baf2a4c046ff6423e43ccb484660d46c5d3d888
Erikdzo <erikdzotsenidze@gmail.com>
2020-09-02 17:28:39 +0300

    #538 Fix overlap with login form on small screens

--- src/components/partial/SiteIcons.vue
+++ src/components/partial/SiteIcons.vue
@@ -1,47 +1,65 @@
 <template>
   <div class="site-icons d-flex justify-center">
+    <!--  Buttons for screens larger than xs  -->
     <v-btn
-      class="align-self-center pa-2"
+      v-for="site in sites"
+      :key="site.key"
+      class="align-self-center pa-2 d-none d-sm-block"
       tile
       depressed
       :ripple="false"
       color="transparent"
       height="fit-content"
       width="fit-content"
-      href="https://natarc.ut.ee"
+      :href="site.url"
       target="window"
     >
       <v-img
         :max-height="100"
         :max-width="100"
-        contains
-        src="../../assets/img/NATARC_transparent.png"
+        contain
+        :src="site.icon"
       ></v-img>
     </v-btn>
+
+    <!--  Buttons for screens size xs  -->
     <v-btn
-      class="align-self-center pa-2"
+      v-for="site in sites"
+      :key="site.key"
+      class="d-block d-sm-none align-self-center pa-2"
       tile
       depressed
       :ripple="false"
       color="transparent"
       height="fit-content"
       width="fit-content"
-      href="https://geocollections.info"
+      :href="site.url"
       target="window"
     >
-      <v-img
-        :max-height="100"
-        :max-width="100"
-        contains
-        src="../../assets/img/sarv_logo.svg"
-      ></v-img>
+      <v-img :max-height="45" :max-width="100" contain :src="site.icon"></v-img>
     </v-btn>
   </div>
 </template>
 
 <script>
 export default {
-  name: "SiteIcons"
+  name: "SiteIcons",
+  data() {
+    return {
+      sites: [
+        {
+          key: "natarc",
+          url: "https://natarc.ut.ee",
+          icon: require("../../assets/img/NATARC_transparent.png")
+        },
+        {
+          key: "sarv",
+          url: "https://geocollections.info",
+          icon: require("../../assets/img/sarv_logo.svg")
+        }
+      ]
+    };
+  }
 };
 </script>
 
