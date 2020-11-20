229484fd78ad4877cb07e9873fea5f4390561484
enar <ents100@gmail.com>
2020-11-20 13:25:27 +0200

    circle ci test

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   // Disable source viewing in production
-  productionSourceMap: true,
-  transpileDependencies: ["vuetify"]
+  productionSourceMap: false,
+  transpileDependencies: ["vuetify"],
+  parallel: process.env.CI ? process.env.CIRCLE_NODE_TOTAL : undefined
 };
