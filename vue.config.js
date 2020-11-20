3fe77ce7e2553715d3c747370dca4ff2d7d61935
enar <ents100@gmail.com>
2020-11-20 13:27:31 +0200

    circle ci typo fix

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ module.exports = {
   // Disable source viewing in production
   productionSourceMap: false,
   transpileDependencies: ["vuetify"],
-  parallel: process.env.CI ? process.env.CIRCLE_NODE_TOTAL : undefined
+  parallel: process.env.CI ? +process.env.CIRCLE_NODE_TOTAL : undefined
 };
