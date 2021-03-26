0386e32a04edf03909ca328bd5a8b9cf950267bc
Enar Mustonen <ents100@gmail.com>
2021-03-26 10:21:16 +0200

    Closes #618 cahnged depth interval translations + now all stratigraphy autocompletes type=1 and litostra. type=2

--- src/mixins/autocompleteMixin.js
+++ src/mixins/autocompleteMixin.js
@@ -106,13 +106,6 @@ const autocompleteMixin = {
     autocompleteStratigraphySearch(value) {
       this.$_autocompleteMixin_search(value, "stratigraphy", "stratigraphy");
     },
-    autocompleteStratigraphyOnlyChronoSearch(value) {
-      this.$_autocompleteMixin_search(
-        value,
-        "stratigraphy_chrono",
-        "stratigraphy"
-      );
-    },
     autocompleteLithostratigraphySearch(value) {
       this.$_autocompleteMixin_search(
         value,
@@ -446,11 +439,9 @@ function buildAutocompleteQuery(type, value, currentUser, groupByField) {
     case "storage":
       return `location/?multi_search=value:${value};fields:location;lookuptype:icontains&fields=id,location`;
     case "stratigraphy":
-      return `stratigraphy/?multi_search=value:${value};fields:id,stratigraphy,stratigraphy_en;lookuptype:icontains&fields=id,stratigraphy,stratigraphy_en`;
-    case "stratigraphy_chrono":
-      return `stratigraphy/?multi_search=value:${value};fields:id,stratigraphy,stratigraphy_en;lookuptype:icontains&type__value_en=chronostratigraphy&fields=id,stratigraphy,stratigraphy_en`;
+      return `stratigraphy/?type=1&multi_search=value:${value};fields:id,stratigraphy,stratigraphy_en;lookuptype:icontains&fields=id,stratigraphy,stratigraphy_en`;
     case "lithostratigraphy":
-      return `stratigraphy/?multi_search=value:${value};fields:id,stratigraphy,stratigraphy_en;lookuptype:icontains&type__value_en=lithostratigraphy&fields=id,stratigraphy,stratigraphy_en`;
+      return `stratigraphy/?type=2&multi_search=value:${value};fields:id,stratigraphy,stratigraphy_en;lookuptype:icontains&type__value_en=lithostratigraphy&fields=id,stratigraphy,stratigraphy_en`;
     case "stratigraphy_top":
     case "stratigraphy_base":
       return `stratigraphy/?multi_search=value:${value};fields:id,stratigraphy,stratigraphy_en;lookuptype:icontains&fields=id,stratigraphy,stratigraphy_en`;
