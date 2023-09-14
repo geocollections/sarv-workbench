8e33d0fe3652361bbf82d5493c0f203a8f642aae
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2023-09-14 10:11:03 +0300

    fix(reference): correctly show digital pdf immediately after adding

--- src/mixins/formManipulation.js
+++ src/mixins/formManipulation.js
@@ -383,17 +383,21 @@ const formManipulation = {
               if (this.$route.meta.object === "reference") {
                 fetchAttachmentForReference(this.$route.params.id).then(
                   (response) => {
-                    const res = this.handleResponse(response);
-                    this.attachment = {
-                      id: res.attachment__id,
-                      attachment_format_value:
-                        res.attachment__attachment_format_value,
-                      author__agent: res.attachment__author__agent,
-                      description: res.attachment__description,
-                      description_en: res.attachment__description_en,
-                      original_filename: res.attachment__original_filename,
-                      uuid_filename: res.attachment__uuid_filename,
-                    };
+                    const res = this.handleResponse(response)[0];
+                    if (res.attachment__id !== null) {
+                      this.attachment = [
+                        {
+                          id: res.attachment__id,
+                          attachment_format_value:
+                            res.attachment__attachment_format_value,
+                          author__agent: res.attachment__author__agent,
+                          description: res.attachment__description,
+                          description_en: res.attachment__description_en,
+                          original_filename: res.attachment__original_filename,
+                          uuid_filename: res.attachment__uuid_filename,
+                        },
+                      ];
+                    }
                   }
                 );
               }
